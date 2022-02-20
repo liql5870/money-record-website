7cc523128ccbff1277cedb785529ff0128decb7f
liql <liqianlong219@gmail.com>
2022-02-20 18:31:51 +0800

    deploy

---
+++ precache-manifest.5401b7c55300478e0f329097f7638b9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "457689ac1804c0d6c9d0",
+    "url": "/money-record-website/css/app.9712acd2.css"
+  },
+  {
+    "revision": "60d6f8b1dc51644fb9c9ba2570a39f52",
+    "url": "/money-record-website/index.html"
+  },
+  {
+    "revision": "457689ac1804c0d6c9d0",
+    "url": "/money-record-website/js/app.6bf74a3b.js"
+  },
+  {
+    "revision": "76e3292617af2445d70f",
+    "url": "/money-record-website/js/chunk-vendors.f9307f9c.js"
+  },
+  {
+    "revision": "28c9b5c389d739d5f2623de1bb59e073",
+    "url": "/money-record-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-record-website/robots.txt"
+  }
+]);
